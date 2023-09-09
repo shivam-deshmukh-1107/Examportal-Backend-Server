d44911636c332685c348a831d7ab117f28c48ad3
Shivam <shivamsdeshmukh1107@gmail.com>
2023-09-09 15:49:55 +0530

    space

--- src/main/java/com/exam/examserver/ExamserverApplication.java
+++ src/main/java/com/exam/examserver/ExamserverApplication.java
@@ -1,12 +1,18 @@
 //Examserver.Application code
 package com.exam.examserver;
 
+import com.exam.examserver.model.Role;
+import com.exam.examserver.model.User;
+import com.exam.examserver.model.UserRole;
 import com.exam.examserver.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class ExamserverApplication implements CommandLineRunner {
@@ -24,27 +30,27 @@ public class ExamserverApplication implements CommandLineRunner {
     public void run(String... args) throws Exception {
         System.out.println("Starting Code!!!!");
 
-//        User user = new User();
-//
-//        user.setFirstname("Shivam");
-//        user.setLastname("Deshmukh");
-//        user.setUsername("shiva");
-//        user.setPassword(this.bCryptPasswordEncoder.encode("Shiva@#786"));
-//        user.setEmail("shivam@gmail.com");
-//        user.setProfile("default.png");
-//        user.setPhone("8879891779");
-//
-//        Role role_1 = new Role();
-//        role_1.setRoleID(44L);
-//        role_1.setRoleName("ADMIN");
-//
-//        Set<UserRole> userRoleSet = new HashSet<>();
-//        UserRole userRole = new UserRole();
-//        userRole.setRole(role_1);
-//        userRole.setUser(user);
-//        userRoleSet.add(userRole);
-//
-//        User user_1 = this.userService.createUser(user, userRoleSet);
+        User user = new User();
+
+        user.setFirstname("Shivam");
+        user.setLastname("Deshmukh");
+        user.setUsername("shiva");
+        user.setPassword(this.bCryptPasswordEncoder.encode("Shiva@#786"));
+        user.setEmail("shivam@gmail.com");
+        user.setProfile("default.png");
+        user.setPhone("8879891779");
+
+        Role role_1 = new Role();
+        role_1.setRoleID(44L);
+        role_1.setRoleName("ADMIN");
+
+        Set<UserRole> userRoleSet = new HashSet<>();
+        UserRole userRole = new UserRole();
+        userRole.setRole(role_1);
+        userRole.setUser(user);
+        userRoleSet.add(userRole);
+
+        User user_1 = this.userService.createUser(user, userRoleSet);
 //            System.out.println(user_1.getUsername());
 
     }
